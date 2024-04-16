889541d4a547f2c0dc05434abf3a7c64ffec6462
kredd <kredd@vailsys.com>
2024-04-16 10:40:12 -0500

    implimenting crud operations in quote searcher

---
+++ S1V4_starter_files/app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const records = require('./records')
+
+app.use (express.json()); 
+
+app.get('/greetings', (req, res)=> {
+    res.json({greeting: "hellow world"})
+});
+
+app.get('/quotes', async (req, res) => {
+  try {
+    const quotes = await records.getQuotes();
+    res.json(quotes);
+  }catch(err){
+    res.json({message:err.messsage})
+  }
+});
+
+app.get('/quotes/:id', async (req, res)=> {
+    try {
+        const quote = await records.getQuote(req.params.id);
+        if(quote) {
+            res.json(quote)
+            
+        }else {
+            res.status(404).json({message: "This doesn't exist"})
+        }
+        res.json(quote);
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+});
+
+app.post('/quotes', (req, res) => {
+    try {
+       if(req.body.author && req.body.quote){
+        const quote = records.createQuote ({
+            quote: req.body,
+            author: req.body.author
+        });
+        res.status(201).json(quote)
+       } else{
+        res.status.json({messgae:"Quote and author required."})
+       }
+      
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+    
+});
+
+app.put('quotes/:id', async (req, res) => {
+    try{
+      const quote = await records.getQuote(req.params.id);
+      if (quote){
+        quote.quote = req.body.quote;
+        quote.author = req.body.author;
+
+       await records.updateQuote(quote);
+       res.status(204).end();
+      } else {
+        res.status(404).json({message: 'Not found'})
+      }
+
+    }catch(err){
+        res.status(500).json({message: "There was an unexpected error"})
+    }
+});
+
+
+app.listen(3000, () => console.log('Quote API listening on port 3000!'));
+
+
